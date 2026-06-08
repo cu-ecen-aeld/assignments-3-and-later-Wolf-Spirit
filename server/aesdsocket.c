b696180f77eca3b4419bfe95838db11411223b70
Alina <abudchenko@whoosh.bike>
2026-06-08 13:23:54 +0300

    Delete comments

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-// #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-// #include <netinet/in.h>
 #include <netdb.h>
-// #include <arpa/inet.h>
-// #include <sys/wait.h>
 #include <signal.h>
 #include <syslog.h>
 #include <stddef.h>
